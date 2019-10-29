08cd345b266cfc73bf6e3ae3e7beb88a80a900c3
Yang Chen <yangchen2048@gmail.com>
2019-10-29 02:57:09 -0400

    Start project

---
+++ db.py
@@ -0,0 +1,17 @@
+from parser import parser
+
+def _db():
+    '''
+        Function: Get sql from GUI, and pass it to paser
+    '''
+    print('__Start DBMS__')
+    # TODO
+
+    print('__End DBMS__')
+
+    pass
+
+
+if __name__ == "__main__":
+    _db()
+    parser()
---
+++ parser.py
@@ -0,0 +1,10 @@
+def parser(_sql=None):
+    '''
+        Funtion: Get sql string, and parse it and call cressponding functions
+    '''
+    print('__Start Parsing__')
+
+    # TODO
+
+    print('__End Parsing__')
+    pass
