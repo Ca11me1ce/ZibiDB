83dc6067b9811b019111c8121a883e8356133c17
Yang Chen <yangchen2048@gmail.com>
2019-11-04 12:59:30 -0500

    Finished phase 1 CREATE DATABASE

--- parser/__init__.py
+++ parser/__init__.py
@@ -18,25 +18,28 @@ def parse(commandline):
 
             # CREATE DATABASE database_name;
             if action[1].upper() == 'DATABASE':
-                database = action[2].replace(';', '')
-                _database='./ZibiDB/database/'
-
-                # If the database is exist, ERROR
-                if os.path.exists(_database+database):
-                    raise Exception('ERROR: The file is exist already.')
-                    
-                # If the database is not exist, create and PASS
-                elif not os.path.exists(_database+database):
-                    os.makedirs(_database+database)
-                    print('PASS: The database is created.')
-                    return
-
-                else:
-                    raise Exception('ERROR: Invalid command.')
+                createDatabase(action)
             else:
-                    raise Exception('ERROR: Only accept CREATE DATABAS.')
+                raise Exception('ERROR: Only accept CREATE DATABAS.')
                 
         else:
-            raise Exception('Only accept exit command now ~~~~ : p')
+            raise Exception('Only accept exit and create database command now ~~~~ : p')
 
     return action
+
+def createDatabase(action):
+    database = action[2].replace(';', '')
+    _database='./ZibiDB/database/'
+
+    # If the database is exist, ERROR
+    if os.path.exists(_database+database):
+        raise Exception('ERROR: The file is exist already.')
+        
+    # If the database is not exist, create and PASS
+    elif not os.path.exists(_database+database):
+        os.makedirs(_database+database)
+        print('PASS: The database is created.')
+        return
+
+    else:
+        raise Exception('ERROR: Invalid command.')
