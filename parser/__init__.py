2b2ce9e58a08d92712bcc23ce17426f81bda89fe
Yang Chen <yangchen2048@gmail.com>
2019-11-04 12:54:02 -0500

    Finished phase 1 CREATE DATABASE

--- parser/__init__.py
+++ parser/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 # pattern
 
 # action
@@ -9,6 +11,32 @@ def parse(commandline):
     if commandline.upper() == 'EXIT':
         action = 'exit'
     else:
-        raise Exception('Only accept exit command now ~~~~ : p')
+        action = commandline.split(' ')
+
+        # CREATE
+        if action[0].upper() == 'CREATE':
+
+            # CREATE DATABASE database_name;
+            if action[1].upper() == 'DATABASE':
+                database = action[2].replace(';', '')
+                _database='./ZibiDB/database/'
+
+                # If the database is exist, ERROR
+                if os.path.exists(_database+database):
+                    raise Exception('ERROR: The file is exist already.')
+                    
+                # If the database is not exist, create and PASS
+                elif not os.path.exists(_database+database):
+                    os.makedirs(_database+database)
+                    print('PASS: The database is created.')
+                    return
+
+                else:
+                    raise Exception('ERROR: Invalid command.')
+            else:
+                    raise Exception('ERROR: Only accept CREATE DATABAS.')
+                
+        else:
+            raise Exception('Only accept exit command now ~~~~ : p')
 
     return action
