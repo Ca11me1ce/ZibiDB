9e0192598ab233459321bf5012d1d3304d5afe31
visual233 <264050673@qq.com>
2019-11-22 14:47:14 -0500

    Add files via upload
    just struct

---
+++ parser/database.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 22 14:41:36 2019
+
+@author: WXS
+"""
+
+class Database:
+    def __init__(self):
+        self.tables = []
+        
+    def save():
+        pass
+    
+    def load():
+        pass
